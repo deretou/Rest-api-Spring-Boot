3ed5e2e1687d31d84e43a3db115ce54ba9ac7f72
deretou <deretou@gmail.com>
2019-11-12 09:20:12 -0600

    API

---
+++ src/main/java/com/example/bootstrap/service/DogService.java
@@ -0,0 +1,13 @@
+package com.example.bootstrap.service;
+
+import com.example.bootstrap.entity.Dog;
+
+
+import java.util.List;
+
+public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogBreedById(Long id);
+    List<String> retrieveDogNames();
+}
