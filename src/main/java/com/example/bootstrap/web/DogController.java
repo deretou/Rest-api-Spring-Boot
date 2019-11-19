e837367a3019a4b40e837201391685285910c24e
deretou <deretou@gmail.com>
2019-11-19 11:38:21 -0600

    Swagger-UI

--- src/main/java/com/example/bootstrap/web/DogController.java
+++ src/main/java/com/example/bootstrap/web/DogController.java
@@ -3,6 +3,8 @@ package com.example.bootstrap.web;
 
 import com.example.bootstrap.entity.Dog;
 import com.example.bootstrap.service.DogService;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@ApiResponses(value = {
+        @ApiResponse(code=400, message = "This is a bad request, please follow the API documentation for the proper request format."),
+        @ApiResponse(code=401, message = "Due to security constraints, your access request cannot be authorized. "),
+        @ApiResponse(code=500, message = "The server is down. Please make sure that the Location microservice is running.")
+})
+
 public class DogController {
     private DogService dogService;
     @Autowired
